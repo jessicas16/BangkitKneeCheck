96760e0bd83ebc11ca151cdcf9a9ec303375235b
ReyKeyS <104489667+ReyKeyS@users.noreply.github.com>
2024-12-04 15:06:59 +0700

    Setting Pasien Nav

---
+++ app/src/main/java/com/example/kneecheck/pasien/ProfilePasienFragment.kt
@@ -0,0 +1,33 @@
+package com.example.kneecheck.pasien
+
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.fragment.app.Fragment
+import com.example.kneecheck.databinding.FragmentProfilePasienBinding
+
+class ProfilePasienFragment : Fragment() {
+
+    private var _binding: FragmentProfilePasienBinding? = null
+
+    // This property is only valid between onCreateView and
+    // onDestroyView.
+    private val binding get() = _binding!!
+
+    override fun onCreateView(
+        inflater: LayoutInflater,
+        container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View {
+        _binding = FragmentProfilePasienBinding.inflate(inflater, container, false)
+        val root: View = binding.root
+
+        return root
+    }
+
+    override fun onDestroyView() {
+        super.onDestroyView()
+        _binding = null
+    }
+}
