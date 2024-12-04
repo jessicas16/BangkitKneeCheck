cb17be78bc0dcfc164457265a4129e674a331c48
ReyKeyS <104489667+ReyKeyS@users.noreply.github.com>
2024-12-04 17:52:35 +0700

    Add tampilan pasien

--- app/src/main/java/com/example/kneecheck/pasien/ScanPasienFragment.kt
+++ app/src/main/java/com/example/kneecheck/pasien/ScanPasienFragment.kt
@@ -5,15 +5,22 @@ import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import androidx.fragment.app.Fragment
+import com.example.kneecheck.config.ApiConfiguration
+import com.example.kneecheck.config.DefaultRepo
 import com.example.kneecheck.databinding.FragmentScanPasienBinding
+import kotlinx.coroutines.CoroutineScope
+import kotlinx.coroutines.Dispatchers
 
 class ScanPasienFragment : Fragment() {
-
     private var _binding: FragmentScanPasienBinding? = null
-
-    // This property is only valid between onCreateView and
-    // onDestroyView.
     private val binding get() = _binding!!
+    private var repo: DefaultRepo = ApiConfiguration.defaultRepo
+    private val ioScope: CoroutineScope = CoroutineScope(Dispatchers.IO)
+    private val mainScope: CoroutineScope = CoroutineScope(Dispatchers.Main)
+
+    private lateinit var id :String
+    private lateinit var name :String
+    private lateinit var token :String
 
     override fun onCreateView(
         inflater: LayoutInflater,
@@ -23,6 +30,12 @@ class ScanPasienFragment : Fragment() {
 
         _binding = FragmentScanPasienBinding.inflate(inflater, container, false)
         val root: View = binding.root
+
+        id = requireActivity().intent.getStringExtra("id").toString()
+        name = requireActivity().intent.getStringExtra("name").toString()
+        token = requireActivity().intent.getStringExtra("token").toString()
+
+        binding.tvNamaPasien.text = name
 
         return root
     }
