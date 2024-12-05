0551386bc3fc05e48e1b48da8a221ab18c145f10
jessicas16 <jessica.susanto16@gmail.com>
2024-12-06 00:21:43 +0700

    add new pasien done

--- app/src/main/java/com/example/kneecheck/dokter/DataPasienDanHasilPredictFragment.kt
+++ app/src/main/java/com/example/kneecheck/dokter/DataPasienDanHasilPredictFragment.kt
@@ -5,15 +5,50 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.core.view.isVisible
 import com.example.kneecheck.R
+import com.example.kneecheck.databinding.FragmentDataPasienDanHasilPredictBinding
+import java.text.SimpleDateFormat
+import java.util.Date
+import java.util.Locale
 
 class DataPasienDanHasilPredictFragment : Fragment() {
+    private lateinit var binding : FragmentDataPasienDanHasilPredictBinding
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
-        // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_data_pasien_dan_hasil_predict, container, false)
+        binding = FragmentDataPasienDanHasilPredictBinding.inflate(inflater, container, false)
+        val root = binding.root
+
+        val confidenceScore = arguments?.getInt("confidenceScore")
+        val label = arguments?.getString("label")
+        val id_xray = arguments?.getString("id_xray")
+        val img_path = arguments?.getString("img_path")
+        val pengobatan = arguments?.getString("pengobatan")
+        val name = arguments?.getString("name")
+        val gender = arguments?.getString("gender")
+        val birth = arguments?.getString("birth")
+        val address = arguments?.getString("address")
+        val akun = arguments?.getString("akun")
+        
+        binding.tvTanggalScanData.text = SimpleDateFormat("dd-MM-yyyy", Locale.getDefault()).format(Date())
+        binding.tvLabelKeparahanData.text = label
+        binding.tvIdXrayData.text = id_xray
+
+        if (akun == "belum"){
+            binding.tvIdPasien.isVisible = false
+            binding.tvIdPasienData.isVisible = false
+        } else {
+            binding.tvIdPasienData.text = arguments?.getString("id_pasien")
+        }
+
+        binding.tvNamaLengkapPasienData.text = name
+        binding.tvBirthPasienData.text = birth
+        binding.tvGenderPasienData.text = gender
+        binding.tvAddressPasienData.text = address
+
+        return root
     }
 
 }
--- app/src/main/java/com/example/kneecheck/dokter/PasienBelumPunyaAkunFragment.kt
+++ app/src/main/java/com/example/kneecheck/dokter/PasienBelumPunyaAkunFragment.kt
@@ -5,16 +5,49 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.navigation.fragment.findNavController
 import com.example.kneecheck.R
+import com.example.kneecheck.databinding.FragmentPasienBelumPunyaAkunBinding
 
 class PasienBelumPunyaAkunFragment : Fragment() {
-
+    private lateinit var binding : FragmentPasienBelumPunyaAkunBinding
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
-        // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_pasien_belum_punya_akun, container, false)
+        binding = FragmentPasienBelumPunyaAkunBinding.inflate(inflater, container, false)
+        val root = binding.root
+
+        val confidenceScore = arguments?.getInt("confidenceScore")!!
+        val label = arguments?.getString("label")!!
+        val id_xray = arguments?.getString("id_xray")!!
+        val img_path = arguments?.getString("img_path")!!
+        val pengobatan = arguments?.getString("pengobatan")!!
+
+        binding.imageButton.setOnClickListener {
+            val name = binding.etNamaPasienBelumPunyaAkun.text.toString()
+            val gender = binding.etGenderPasienBelumPunyaAkun.text.toString()
+            val birth = binding.etBirthPasienBelumPunyaAkun.text.toString()
+            val domisili = binding.etKotaPasienBelumPunyaAkun.text.toString()
+
+            // Arahkan ke halaman pendaftaran
+            val bundle = Bundle()
+            bundle.putInt("confidenceScore", confidenceScore)
+            bundle.putString("label", label)
+            bundle.putString("id_xray", id_xray)
+            bundle.putString("img_path", img_path)
+            bundle.putString("pengobatan", pengobatan)
+            bundle.putString("name", name)
+            bundle.putString("gender", gender)
+            bundle.putString("birth", birth)
+            bundle.putString("address", domisili)
+            bundle.putString("akun", "belum")
+
+            findNavController().navigate(R.id.action_pasienBelumPunyaAkunFragment_to_dataPasienDanHasilPredictFragment, bundle)
+
+        }
+
+        return root
     }
 
 }
